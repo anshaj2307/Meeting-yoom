3a1c4198e7556223ecb02ca13505a67beaae8a99
anshaj2307 <anshaj852@gmail.com>
2025-06-27 16:28:58 +0530

    First commit

---
+++ config/index.ts
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Yoom",
+  description: "Meeting app with realtime video, audio, and chat features.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "getstream",
+    "stream",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "zoom-clone",
+    "realtime-video",
+    "realtime-chat",
+    "live-audio",
+    "live-video",
+    "live-chat",
+    "radix-toast",
+    "lucide-react",
+    "next-themes",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "ts",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Anshaj Srivastava",
+    url: "https://github.com/anshaj2307",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/anshaj2307/Meeting-yoom.git",
+} as const;
